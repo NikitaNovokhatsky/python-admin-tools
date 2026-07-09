942ee8c231e4a1344c6999dffabff5c08bd7da92
NikitaNovokhatsky <Sempay.nik@mail.ru>
2026-07-09 12:08:53 +0300

    Add CSV reports and application version

--- ping_checker/ping_checker.py
+++ ping_checker/ping_checker.py
@@ -1,6 +1,11 @@
 import subprocess
 import argparse
 import logging
+import csv
+from datetime import datetime
+from unittest import result
+from urllib.parse import _ResultMixinStr
+VERSION = "1.0.0"
 
 
 def check_host(host):
@@ -25,17 +30,35 @@ def load_hosts(filename):
             if line.strip()
         ]
 
+def save_report(results):
+    filename = "ping_report.csv"
+
+    with open(filename, "w", newline="") as file:
+        writer = csv.writer(file)
+
+        writer.writerow(
+            ["Host", "Status", "Time"]
+        )
+
+        for result in results:
+            writer.writerow(result)
+
 def main():
     parser = argparse.ArgumentParser(
         description="Check host availability"
     )
 
     parser.add_argument(
+        "--version",
+    action="version",
+    version=f"%(prog)s {VERSION}"
+    )
+    parser.add_argument(
         "host",
         nargs="?",
         help="IP address or domain name"
     )
-
+    
     parser.add_argument(
         "-f",
         "--file",
@@ -44,7 +67,8 @@ def main():
 
     args = parser.parse_args()
 
-    hosts = []
+    host = []
+    results = []
 
     if args.file:
         hosts = load_hosts(args.file)
@@ -57,11 +81,22 @@ def main():
         return
     
     for host in hosts:
+        save_report(results)
 
         if check_host(host):
-            print(f"{hosts} is ONLINE")
+            status = "ONLINE"
         else:
-            print(f"{host} is OFFLINE")
+            status = "OFFLINE"
+
+        print(f"{host} is {status}")
+
+        results.append(
+            [host,
+             status,
+             datetime.now()
+             ]
+    
+        )
 
 
 logging.basicConfig(
